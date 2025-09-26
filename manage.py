bc5c4830e7b70cf56c1ed6dbf46ecfcac5f4ddef
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-26 15:10:13 +0330

    Update Django settings module for consistency

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'du_an_an_ly_du_lieu.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
