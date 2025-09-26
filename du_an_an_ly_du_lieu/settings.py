d4bedb92bc45a5aaa240ce89560669c7af7b020d
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-26 15:08:40 +0330

    feat: initial commit for du_an_an_ly_du_lieu/settings.py

---
+++ du_an_an_ly_du_lieu/settings.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+# Đường dẫn cơ bản
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Ứng dụng cài đặt
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+# Khởi tạo cơ sở dữ liệu
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# URL route
+ROOT_URLCONF = 'du_an_an_ly_du_lieu.urls'
+
+# Tạo template
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Mặc định phát triển
+DEBUG = True
+
+# URL tĩnh
+STATIC_URL = '/static/'
